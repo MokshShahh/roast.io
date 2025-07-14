2e20ca1efadda941c871b9c9f5066c13bb0b7af8
Moksh <moksh13112006@gmail.com>
2025-07-15 01:37:04 +0530

    github roasting done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,23 +5,19 @@ import '../App.css';
 function Hero() {
     const navigate = useNavigate();
 
-    const handleClick = () => {
-        navigate('/loading');
-    };
-
     return (
         <div className="hero">
-            <h1>From Commits to Cringe:<br />Let’s Review You</h1>
+            <h1>From Commits To Cringe:<br />Let’s Review You</h1>
             <p className="subtitle">Your connections won't save you now.<br />Not even that VP in your network.</p>
-            <button className="cta" onClick={handleClick}>
+            <button className="cta" onClick={()=>{navigate('./loading')}}>
                 Find Out
             </button>
             <br />
             <img src={horizontal} alt="Logo" style={{ width: '700px', marginTop: '10px' }} />
             <div className="footer-wrapper">
                 <div className="footer-bar">
-                    <span className="small-text">a project by </span>
-                    <span className="big-text">GDSC</span>
+                    <div className="small-text">A project by </div>
+                    <div className="big-text text-align-right">DCxGDG</div>
                 </div>
                 <div className="footer-main">
                     © {new Date().getFullYear()} roast.io
